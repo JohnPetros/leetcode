d548a217666a98879738ae62cefc511d7f1be79c
JohnPetros <joaopedro.nc@outlook.com>
2025-03-17 16:34:57 -0300

    🔢 array: solve product of array except self again

--- array/product_of_array_except_self.py
+++ array/product_of_array_except_self.py
@@ -4,13 +4,42 @@ def product_of_array_except_self(numbers: list[int]) -> list[int]:
 
     for number in numbers:
         products.append(previous_product)
+        previous_product *= number
+
+    previous_product = 1
+
+    for index in range(len(numbers) - 1, -1, -1):
+        products[index] *= previous_product
+        previous_product *= numbers[index]
+
+    return products
+
+
+def _product_of_array_except_self(numbers: list[int]) -> list[int]:
+    previous_product = 1
+    prefix_products = []
+    postfix_products = []
+    products = []
+
+    for number in numbers:
+        prefix_products.append(previous_product * number)
         previous_product = previous_product * number
 
     previous_product = 1
 
     for index in range(len(numbers) - 1, -1, -1):
-        products[index] = products[index] * previous_product
+        postfix_products.append(previous_product * numbers[index])
         previous_product = previous_product * numbers[index]
+
+    postfix_products.reverse()
+
+    for index in range(len(numbers)):
+        if index == 0:
+            products.append(postfix_products[index + 1])
+        elif index == len(numbers) - 1:
+            products.append(prefix_products[index - 1])
+        else:
+            products.append(prefix_products[index - 1] * postfix_products[index + 1])
 
     return products
 
