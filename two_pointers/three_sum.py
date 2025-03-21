0a00a879742e2d6dfba5036fa06ef3ab415c953b
JohnPetros <joaopedro.nc@outlook.com>
2025-03-21 15:38:50 -0300

    🔁 two pointers: solve three sum

--- two_pointers/three_sum.py
+++ two_pointers/three_sum.py
@@ -1,30 +1,23 @@
-def three_sum(numbers: list[int]) -> list[list[int]]:
+def three_sum(numbers: list[int]):
     numbers.sort()
-    sums = []
+    triplets = []
 
-    for start in range(len(numbers) - 2):
-        if numbers[start] > 0:
+    for index in range(len(numbers)):
+        if numbers[index] > 0:
             break
-
-        if start > 0 and numbers[start] == numbers[start - 1]:
+        elif index > 0 and numbers[index] == numbers[index - 1]:
             continue
 
-        left_pointer = start + 1
+        left_pointer = index + 1
         right_pointer = len(numbers) - 1
 
         while left_pointer < right_pointer:
-            combination = [
-                numbers[start],
-                numbers[left_pointer],
-                numbers[right_pointer],
-            ]
-
-            current_sum = sum(combination)
-            if current_sum == 0:
-                sums.append(combination)
+            tripplet = [numbers[index], numbers[left_pointer], numbers[right_pointer]]
+            total = sum(tripplet)
+            if total == 0:
+                triplets.append(tripplet)
                 left_pointer += 1
                 right_pointer -= 1
-
                 while (
                     left_pointer < right_pointer
                     and numbers[left_pointer] == numbers[left_pointer - 1]
@@ -35,17 +28,15 @@ def three_sum(numbers: list[int]) -> list[list[int]]:
                     and numbers[right_pointer] == numbers[right_pointer + 1]
                 ):
                     right_pointer -= 1
-            elif current_sum < 0:
+            elif total < 0:
                 left_pointer += 1
             else:
                 right_pointer -= 1
 
-    return sums
+    return triplets
 
 
-# print(three_sum([-3, -3, -2, -1, 0, 1, 2, 2, 3]))
 # print(three_sum([-1, 0, 1, 2, -1, -4]))
 # print(three_sum([0, 1, 1]))
-# print(three_sum([2, 4, 1, 0, -3, -1]))
-print(three_sum([0, 0, 0]))
-# print(three_sum([-1, 0, 1, 2, -1, -4]))
+# print(three_sum([0, 0, 0]))
+print(three_sum([0, 0, 0, 0]))
