717fa7bdff4c62d2cbc625e51f5ef093daf1228a
JohnPetros <joaopedro.nc@outlook.com>
2025-04-22 16:10:31 -0300

    🔍 binary search: solve koko eating bananas

---
+++ binary_search/koko_eating_bananas.py
@@ -0,0 +1,21 @@
+def koko_eating_bananas(piles: list[int], hours: int) -> int:
+    left_pointer = 1
+    right_pointer = max(piles)
+
+    while left_pointer < right_pointer:
+        middle_pointer = (left_pointer + right_pointer) // 2
+        total_hours = sum(
+            (pile + middle_pointer - 1) // middle_pointer for pile in piles
+        )
+
+        if total_hours <= hours:
+            right_pointer = middle_pointer
+        else:
+            left_pointer = middle_pointer + 1
+
+    return left_pointer
+
+
+print(koko_eating_bananas([3, 6, 7, 11], 8))
+print(koko_eating_bananas([30, 11, 23, 4, 20], 5))
+print(koko_eating_bananas([30, 11, 23, 4, 20], 6))
