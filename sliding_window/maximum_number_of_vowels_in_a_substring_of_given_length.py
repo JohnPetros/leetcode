4ecf9b0c15d817bc646847de86146c52b8e2ac04
JohnPetros <joaopcarvalho.cds@gmail.com>
2026-01-03 11:19:54 -0300

    🔢 sliding window: refactor maximum_number_of_vowels_in_a_substring_of_given_length function for clarity and efficiency

--- sliding_window/maximum_number_of_vowels_in_a_substring_of_given_length.py
+++ sliding_window/maximum_number_of_vowels_in_a_substring_of_given_length.py
@@ -1,32 +1,18 @@
 def maximum_number_of_vowels_in_a_substring_of_given_length(s: str, k: int) -> int:
-    n = len(s)
-    vowels = set("aeiou")
-    if n == 1:
-        if s in vowels:
-            return 1
-        else:
-            return 0
+    vowels = "aeiou"
+    maximum = count = left = 0
 
-    maximum = 0
-    left = 0
-    count = 0
+    for right in range(len(s)):
+        if s[right] in vowels:
+            count += 1
 
-    for right in range(n):
-        if right < k - 1:
-            if s[right] in vowels:
-                count += 1
-                maximum = max(maximum, count)
-        else:
-            if s[right] in vowels:
-                count += 1
-                maximum = max(maximum, count)
-
+        if right >= k:
             if s[left] in vowels:
                 count -= 1
-                maximum = max(maximum, count)
-                left += 1
-            else:
-                left += 1
+            left += 1
+
+        if count > maximum:
+            maximum = count
 
     return maximum
 
